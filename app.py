a79e233be587420ddb8744704137ffdfa409d126
LI Yiwen529 <yimanlee529@gmail.com>
2025-02-19 13:17:21 +0800

    backend API by Python for filtering a single SMS

--- app.py
+++ app.py
@@ -62,5 +62,65 @@ def filter():
                }
                unselectedSMS.append(unselected_data)
     return jsonify({"selectedSMS":selectedSMS,"unselectedSMS":unselectedSMS})
+# 在手機處於運行狀態時收到了一條SMS，插入MongoDB以及做篩選
+@app.route('/filter/one/sms', methods=['POST'])
+def filterOne():
+    data=request.get_json()
+    tasks=data.get('tasks',[])
+    keywords=data.get('keywords',[])
+    sms=data.get('sms',{})
+    def have_common_item(list1, list2):
+        for i in list1:
+            for j in list2:  
+                if i in j["task"]:
+                    return {"email": j['email'], "status": True}
+        return {"email": "", "status": False}
+    def common(list1, list2):
+        for i in list1:
+            for j in list2:  
+                if i in j:
+                    return True
+        return False
+    sms_object={}
+    if sms:
+        result=SMS.insert_one(sms)
+        inserted_id=str(result.inserted_id)
+        if any('\u4e00' <= char <= '\u9fff' for char in sms['smsBody']):
+            chinese_words = list(jieba.lcut(sms['smsBody']))
+            if have_common_item(chinese_words,tasks)['status'] & common(chinese_words,keywords):
+               sms_object={
+                   "_id": inserted_id,
+                    "phoneNo": sms["phoneNo"],
+                    "smsBody": sms['smsBody'],
+                    "email": have_common_item(chinese_words,tasks)['email'],
+                    "selected": True
+               }
+            else:
+                sms_object={
+                   "_id": inserted_id,
+                    "phoneNo": sms["phoneNo"],
+                    "smsBody": sms['smsBody'],
+                    "selected": False
+               }
+        else:
+            english_words = sms['smsBody'].split()
+            if have_common_item(english_words, tasks)['status']& common(english_words, keywords):
+                sms_object={
+                    "_id": inserted_id,
+                    "phoneNo": sms["phoneNo"],
+                    "smsBody": sms['smsBody'],
+                    "email": have_common_item(chinese_words,tasks)['email'],
+                    "selected": True
+                }
+            else:
+                sms_object={
+                    "_id": inserted_id,
+                    "phoneNo": sms["phoneNo"],
+                    "smsBody": sms['smsBody'],
+                    "selected": False
+                }
+        return jsonify({"message": sms_object})
+    else:
+        return jsonify({"message": "SMS is empty"})
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
